e4e83938686d5047157d8a3b779e6d3a04c043dd
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-09-02 12:54:27 -0400

    Update ols_review.py

--- ols_review.py
+++ ols_review.py
@@ -7,5 +7,5 @@ results = smf.ols('Lottery ~ Literacy + np.log(Pop1831)', data=dat).fit()
 print(results.summary())
 
 
-#Results are in ols_review.py
+#Results are in ols_review.txt
 #Source: Seabold, Skipper, and Josef Perktold. “Statsmodels: Econometric and statistical modeling with python.” Proceedings of the 9th Python in Science Conference. 2010.
