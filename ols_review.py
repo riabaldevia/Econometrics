8c068b4f7d7187b8f4e35e60acbf7c21cc67626c
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-09-02 12:52:14 -0400

    Create ols_review.py

---
+++ ols_review.py
@@ -0,0 +1,11 @@
+import numpy as np
+import statsmodels.api as sm
+from pandas.core import datetools
+import statsmodels.formula.api as smf
+dat = sm.datasets.get_rdataset("Guerry", "HistData").data
+results = smf.ols('Lottery ~ Literacy + np.log(Pop1831)', data=dat).fit()
+print(results.summary())
+
+
+#Results are in ols_review.py
+#Source: Seabold, Skipper, and Josef Perktold. “Statsmodels: Econometric and statistical modeling with python.” Proceedings of the 9th Python in Science Conference. 2010.
