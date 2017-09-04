fb67b0729a92d04bcff6a72a81829ff0e49f3136
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-09-03 23:37:35 -0400

    Create linearregression.py

---
+++ quantecon/linearregression.py
@@ -0,0 +1,5 @@
+#Source: https://lectures.quantecon.org/py/ols.html
+
+import pandas as pd
+df1 = pd.read_stata('https://github.com/QuantEcon/QuantEcon.lectures.code/raw/master/ols/maketable1.dta')
+df1.head()
