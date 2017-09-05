f09f3ef88b625ac59e602e0dad3c3f0bd5079ddf
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-09-04 23:29:52 -0400

    Update linearregression.py

--- quantecon/linearregression.py
+++ quantecon/linearregression.py
@@ -3,3 +3,20 @@
 import pandas as pd
 df1 = pd.read_stata('https://github.com/QuantEcon/QuantEcon.lectures.code/raw/master/ols/maketable1.dta')
 df1.head()
+
+#Output
+
+ shortnam   euro1900  excolony    avexpr  logpgp95  cons1  cons90  democ00a  \
+0      AFG   0.000000         1       NaN       NaN      1       2         1   
+1      AGO   8.000000         1  5.363636  7.770645      3       3         0   
+2      ARE   0.000000         1  7.181818  9.804219    NaN     NaN       NaN   
+3      ARG  60.000004         1  6.386364  9.133459      1       6         3   
+4      ARM   0.000000         0       NaN  7.682482    NaN     NaN       NaN   
+
+   cons00a    extmort4    logem4  loghjypl  baseco  
+0        1   93.699997  4.540098       NaN     NaN  
+1        1  280.000000  5.634789 -3.411248       1  
+2      NaN         NaN       NaN       NaN     NaN  
+3        3   68.900002  4.232656 -0.872274       1  
+4      NaN         NaN       NaN       NaN     NaN  
+
