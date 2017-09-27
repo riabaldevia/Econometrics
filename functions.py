36716fb195832aa2d5e4ea1730c451da041c0e92
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-09-26 23:54:28 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+#Functions are declared using the def keyword, and the value produced is returned using the
+#return keyword. Consider a simple function which returns the square of the input, y = x^2.
+
 from __future__ import print_function, division
 def square(x):
 return x**2
