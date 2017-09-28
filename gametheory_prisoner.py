d489e5a1eb531d2ff5b5515fced61dd0f7a43048
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-09-27 23:32:28 -0400

    Update gametheory_prisoner.py

--- gametheory_prisoner.py
+++ gametheory_prisoner.py
@@ -1,1 +1,12 @@
 #Prisoners Dilemma
+import numpy as np
+import quantecon.game_theory as gt
+
+g_PD = gt.NormalFormGame((2, 2))  # There are 2 players, each of whom has 2 actions
+g_PD[0, 0] = 1, 1
+g_PD[0, 1] = -2, 3
+g_PD[1, 0] = 3, -2
+g_PD[1, 1] = 0, 0
+
+
+print(g_PD)
