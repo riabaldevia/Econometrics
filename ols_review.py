17e200a8fc1abe49efdba26d80270c80783fd893
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-09-28 23:52:25 -0400

    Update ols_review.py

--- ols_review.py
+++ ols_review.py
@@ -1,3 +1,4 @@
+#Example 1
 import numpy as np
 import statsmodels.api as sm
 from pandas.core import datetools
@@ -9,3 +10,16 @@ print(results.summary())
 
 #Results are in ols_review.txt
 #Source: Seabold, Skipper, and Josef Perktold. “Statsmodels: Econometric and statistical modeling with python.” Proceedings of the 9th Python in Science Conference. 2010.
+
+
+#Example 2
+
+import numpy as np
+import pandas as pd
+
+# load statsmodels as alias ``sm``
+import statsmodels.api as sm
+
+# load a dataset into a pandas data frame - first column (year) used as row labels
+df = pd.read_csv('url of dataset', index_col=0)
+df.head()
