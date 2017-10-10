be6f71276fb9a4d0715b126620099cf23aa51562
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-10-09 22:27:56 -0400

    Update chapter_2.py

--- chapter_2.py
+++ chapter_2.py
@@ -1,6 +1,6 @@
 # Number 3: Analyze data from the National longitudinal Survey of Youth 1979 cohort. 
-# Data source: https://www.bls.gov/nls/nlsy79.htm
+# Data source: https://www.bls.gov/nls/nlsy79.htm & https://www.bls.gov/nls/y79supp.htm
 # 1. Summary
 # 2. Create scatterplot of wages and adult height.
 # 3. Create scatterplot of wages and adult height that excludes the observations with wages above $500 per hour. 
-#4. Create a scatterplot of adult height against adolescent height. Identify the set of observations where people's adolescent height is less than their adult height. Do you think we should use these observations in any future analysis we conduct with this data? 
+# 4. Create a scatterplot of adult height against adolescent height. Identify the set of observations where people's adolescent height is less than their adult height. Do you think we should use these observations in any future analysis we conduct with this data? 
