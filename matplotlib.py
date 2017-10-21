7b90726542bae7f9a4c38160a16385271ca53847
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-10-20 23:35:54 -0400

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -11,3 +11,15 @@ plt.plot(x, np.cos(x))
 plt.show()
 
 #What would be a good example for this problem? 
+
+
+#2
+
+import matplotlib.pyplot as plt
+import numpy as np
+fig, ax = plt.subplots()
+x = np.linspace(0, 100, 2500)
+y = np.sin(x)
+ax.plot(x, y, 'r-', linewidth=4, label='sine function', alpha=0.6)
+ax.legend()
+plt.show()
