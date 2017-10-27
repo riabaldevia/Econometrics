a9a7f3d16b1c1e896020b75662e321fed3404ed6
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-10-26 22:44:57 -0400

    Update linearregression.py

--- quantecon/linearregression.py
+++ quantecon/linearregression.py
@@ -56,7 +56,7 @@ model = LinearRegression()
 model.fit(x, y)
 
 # predict y from the data
-x_new = np.linspace(0, 30, 100)
+x_new = np.linspace(0, 100, 100)
 y_new = model.predict(x_new[:, np.newaxis])
 
 # plot the results
