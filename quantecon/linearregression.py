c1bb38607f7daf5f7c890427d8d315bea6858691
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-10-28 19:13:18 -0500

    Update linearregression.py

--- quantecon/linearregression.py
+++ quantecon/linearregression.py
@@ -73,3 +73,37 @@ ax.axis('tight')
 
 plt.show()
 
+
+#Example 4
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# x from 0 to 100
+x = 100 * np.random.random((10, 1))
+
+# y = a*x + b with noise
+y = 0.25 * x + 2.0 + np.random.normal(size=x.shape)
+
+# create a linear regression model
+model = LinearRegression()
+model.fit(x, y)
+
+# predict y from the data
+x_new = np.linspace(0, 1000, 150)
+y_new = model.predict(x_new[:, np.newaxis])
+
+# plot the results
+plt.figure(figsize=(4, 3))
+ax = plt.axes()
+ax.scatter(x, y)
+ax.plot(x_new, y_new)
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+
+ax.axis('tight')
+
+
+plt.show()
+
