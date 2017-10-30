a3a3946c7669e664bf44f39162ee2bbccb1f32fc
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-10-29 20:56:54 -0500

    Update bmi.py

--- quantecon/bmi.py
+++ quantecon/bmi.py
@@ -2,13 +2,15 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-dataframe = pd.read_csv('data/bmi_and_life_expectancy.csv')
+#read data
+dataframe = pd.read_csv('quantecon_data/bmi.csv')
 x_values = dataframe[['BMI']] 
 y_values = dataframe[['Life expectancy']]
 
+#train data
 model = LinearRegression()
 model.fit(x_values, y_values)
 
-
+#visualize the data
 plt.scatter(x_values, y_values)
 plt.plot(x_values, model.predict(x_values))
