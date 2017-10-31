bee16531720a538c872e1cabf8aff6c55cef4809
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-10-30 22:46:26 -0500

    Update linear_algebra.py

--- quantecon/linear_algebra.py
+++ quantecon/linear_algebra.py
@@ -3,14 +3,11 @@
 import numpy as np
 a = np.array([2, 5, 1]) #This tuple is now an array
 a
-
 array([2, 5, 1])
-
 
 b = np.ones(3) #numpy.ones(shape, dtype=None, order='C'). Return a new array of given shape and type, filled with ones.
 a + b
 array([ 3.,  6.,  2.])
-
 
 3*a #multiple arry([2, 5, 1]) by 3
 array([ 6, 15,  3])
